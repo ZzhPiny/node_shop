1ff0a2e99f62a5af717e67da4654f1e5d6427d78
Piny <zzhtxdy@gmail.com>
2017-07-06 12:56:31 +0800

    删除node_module文件夹，只保留package

--- public/src/js/admin/components/pagesetting/home.jsx
+++ public/src/js/admin/components/pagesetting/home.jsx
@@ -4,14 +4,46 @@
  */
 import React, { Component } from 'react';
 
-import {Layout} from 'antd/dist/antd';
+import { Collapse } from 'antd/dist/antd';
+const Panel = Collapse.Panel;
+
+const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`;
+
+const customPanelStyle = {
+  // background: '#f7f7f7',
+  borderRadius: 4,
+  marginBottom: 24,
+  border: 0,
+};
 
 export default class extends Component {
     render() {
         return (
-            <div>
-                网站主页
-            </div>
+            <Collapse bordered={false} defaultActiveKey={['1']}>
+                <Panel header={<div>轮播图设置</div>} key="1" style={customPanelStyle}>
+                    <div>{text}</div>
+                    <p>{text}</p>
+                </Panel>
+                <Panel header={<span>产品展示设置</span>} key="2" style={customPanelStyle}>
+                    <p>{text}</p>
+                </Panel>
+                <Panel header={<span>横幅设置</span>} key="3" style={customPanelStyle}>
+                    <p>{text}</p>
+                </Panel>
+                <Panel header={<span>博客资讯设置</span>} key="4" style={customPanelStyle}>
+                    <p>{text}</p>
+                </Panel>
+                <Panel header={<span>关于我们设置</span>} key="5" style={customPanelStyle}>
+                    <p>{text}</p>
+                </Panel>
+                <Panel header={<span>其他设置</span>} key="6" style={customPanelStyle}>
+                    <p>{text}</p>
+                </Panel>
+            </Collapse>
         )
     }
 }
